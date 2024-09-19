30d70b5ff27a13db14b7841a3aa654891e12a9d7
tubi55 <tubi55@nate.com>
2024-09-19 14:47:02 +0900

    문자열 자르는 함수 정의

--- script.js
+++ script.js
@@ -1,31 +1,14 @@
-console.log("test");
+shortenText("h1", 5);
+shortenText("h2", 5);
+shortenText("h3", 20);
 
-const h1 = document.querySelector("h1");
-h1.innerText = "Test";
+function shortenText(elem, len) {
+  //첫 번째 인수로 받은 문자값으로 DOM요소 선택
+  const el = document.querySelector(elem);
+  //해당 요소의 문자값 변수에 할당
+  const el_text = el.innerText;
 
-const h2 = document.querySelector("h2");
-const linkURL = "https://www.naver.com";
-h2.innerHTML = `<a href=${linkURL}>naver</a>`;
-
-const h3 = document.querySelector("h3");
-const h3_text = h3.innerText;
-//h3문자열을 0위치에서부터 50번째 위치까지 짜른뒤
-//짜른 문자열을 다시 h3.innerText 프로퍼티에 대입해서 적용
-
-/*
-if (h3_text.length > 30) {
-  h3.innerText = h3_text.substring(0, 30) + "...";
-} else {
-  h3.innerText = h3_text;
+  //해당 돔요소에 innerText 프로퍼티에 대입되는 우항을 3항연산자로 연산 처리후 대입
+  el.innerText =
+    el_text.length > len ? el_text.substring(0, len) + "..." : el_text;
 }
-*/
-
-/*
-if (h3_text.length > 30) h3.innerText = h3_text.substring(0, 30) + "...";
-else h3.innerText = h3_text;
-*/
-
-h3.innerText = h3_text.length > 30 ? h3_text.substring(0, 30) + "..." : h3_text;
-
-//미션 - 위의 기능을 재사용하기 편하도록 함수로 패키징
-// shortenText('h3', 30); 9시 30분까지
